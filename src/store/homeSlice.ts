2651534149cb9030f244a0792e37ddf284237c8b
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-05-11 18:03:22 +0530

    redux devtool setup started

---
+++ src/store/homeSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface CounterState {
+  url: object,
+  genres: object
+}
+
+const initialState: CounterState = {
+  url: {},
+  genres: {}
+}
+
+export const homeSlice = createSlice({
+  name: 'home',
+  initialState,
+  reducers: {
+    getApiConfiguration: (state , action: PayloadAction<object>) =>{
+        state.url = action.payload
+    },
+    getGenres: (state , action: PayloadAction<object>) => {
+        state.genres = action.payload
+    }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { getApiConfiguration , getGenres } = homeSlice.actions
+
+export default homeSlice.reducer
