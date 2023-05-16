cb616d32427e406e9e6474a7cce3ec1f885705da
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-05-16 22:58:05 +0530

    search results page complete

---
+++ src/components/movieCard/MovieCard.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import "./style.scss";
+import Img from "../lazyLoadImage/Img";
+import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
+import PosterFallback from "../../assets/no-poster.png";
+import { RootState } from "../../store/store";
+import { ISearchResults } from "../../pages/searchResults/SearchResult";
+
+interface IMovieCard{
+    data:ISearchResults;
+    fromSearch:boolean;
+    mediaType?:string;
+}
+
+const MovieCard : FC<IMovieCard> = ({ data, fromSearch, mediaType }) => {
+    const { url } = useSelector((state:RootState) => state.home);
+    console.log("data",data)
+    const navigate = useNavigate();
+    const posterUrl = data.poster_path
+        ? url.poster + data.poster_path
+        : PosterFallback;
+    return (
+        <div
+            className="movieCard"
+            onClick={() =>
+                navigate(`/${data.media_type || mediaType}/${data.id}`)
+            }
+        >
+            <div className="posterBlock">
+                <Img className="posterImg" src={posterUrl} />
+                {!fromSearch && (
+                    <React.Fragment>
+                        <CircleRating rating={Number(data.vote_average.toFixed(1))} />
+                        <Genres data={data.genre_ids.slice(0, 2)} />
+                    </React.Fragment>
+                )}
+            </div>
+            <div className="textBlock">
+                <span className="title">{data.title || data.name}</span>
+                <span className="date">
+                    {dayjs(data.release_date).format("MMM D, YYYY")}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default MovieCard;
--- src/pages/searchResults/SearchResult.tsx
+++ src/pages/searchResults/SearchResult.tsx
@@ -4,8 +4,10 @@ import "./style.scss";
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import { useParams } from "react-router-dom";
+import Spinner from "../../components/spinner/Spinner";
+import MovieCard from "../../components/movieCard/MovieCard";
 
-interface ISearchResults {
+export interface ISearchResults {
   adult: boolean;
   backdrop_path: string | null;
   first_air_date: string;
@@ -21,6 +23,8 @@ interface ISearchResults {
   poster_path: string | null;
   vote_average: number;
   vote_count: number;
+  title:string;
+  release_date:string;
 }
 
 export interface ISearch {
@@ -46,6 +50,7 @@ function SearchResult() {
   };
 
   useEffect(() => {
+    setPageNum(1)
     fetchInitialData();
   }, [query]);
 
@@ -65,7 +70,33 @@ function SearchResult() {
   };
 
   return (
-    <div className="searchResultsPage">SearchResult</div>
+    <div className="searchResultsPage">
+      {loading && <Spinner initial={true} />}
+      {!loading && (
+        <ContentWrapper>
+          {data && data.results.length > 0 ? (
+            <>
+            <div className="pageTitle">
+              {data && `Search ${data.total_results > 1 ? "results" : "result"} of '${query}'`}
+            </div>
+            <InfiniteScroll className="content" dataLength={data?.results?.length || 0} next={fetchNextPageData} hasMore={pageNum <= data?.total_pages} loader={<Spinner />} >
+              {data?.results.map((item , index) => {
+                if(item.media_type === "person")return;
+
+                return(
+                  <MovieCard key={index} data={item} fromSearch={true} />
+                )
+              })}
+            </InfiniteScroll>
+            </>
+          ):(
+            <span className="resultNotFound">
+              Sorry, Results not found!
+            </span>
+          )}
+        </ContentWrapper>
+      )}
+    </div>
   );
 }
 
